7e1c3938f3888a32e107c708f7b74dcb40f35c98
Giovanni Lerma Suarez <andres.lerma@valid.com>
2021-10-09 17:25:25 -0500

    Class my application added.

---
+++ app/src/main/java/com/example/pruebarappi/application/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.example.pruebarappi.application
+
+import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Fresco.initialize(this)
+    }
+}

8eae4644e5e3e22fddfe4cab0a7367974b00874d
Giovanni Lerma Suarez <andres.lerma@valid.com>
2021-10-09 17:26:59 -0500

    Response service added.

---
+++ app/src/main/java/com/example/pruebarappi/db/model/ResponseService.kt
@@ -0,0 +1,7 @@
+package com.example.pruebarappi.db.model
+
+class ResponseService {
+    val page: Int? = null
+    val results: ArrayList<ResultService>? = null
+    val total_pages: Int? = null
+}

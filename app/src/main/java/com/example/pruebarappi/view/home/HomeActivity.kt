75131b17508efe37308be209b3e0c5687508f745
Giovanni Lerma Suarez <andres.lerma@valid.com>
2021-10-09 19:28:40 -0500

    Getting order with code.

--- app/src/main/java/com/example/pruebarappi/view/home/HomeActivity.kt
+++ app/src/main/java/com/example/pruebarappi/view/home/HomeActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pruebarappi.R
@@ -15,7 +14,9 @@ import com.example.pruebarappi.view.detail.DetailActivity
 import com.example.pruebarappi.view.home.fragment.interfaces.MoviesTvShowInterface
 import com.example.pruebarappi.viewmodel.HomeActivityViewModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.android.synthetic.main.view_bottom_sheet_details.*
 import java.util.*
@@ -33,6 +34,52 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener, MoviesTvShowInte
 
     private val homeActivityViewModel by viewModels<HomeActivityViewModel>()
 
+    private fun consumeMoviesNowPlaying() {
+        homeActivityViewModel.getResponseV(MOVIES_NOW_PLAYING, 1, { listMoviesNowPlaying ->
+            consumeMoviesPopular(listMoviesNowPlaying?.results)
+        }, { error -> Snackbar.make(coordinatorLayout, "$error", Snackbar.LENGTH_SHORT).show() })
+    }
+
+    private fun consumeMoviesPopular(moviesNowPlaying: ArrayList<ResultService>?) {
+        homeActivityViewModel.getResponseV(MOVIES_POPULAR, 1, { listMoviesPopular ->
+            consumeTvAiringToday(moviesNowPlaying, listMoviesPopular?.results)
+        }, { error -> Snackbar.make(coordinatorLayout, "$error", Snackbar.LENGTH_SHORT).show() })
+    }
+
+    private fun consumeTvAiringToday(
+        moviesNowPlaying: ArrayList<ResultService>?,
+        moviesPopular: ArrayList<ResultService>?
+    ) {
+        homeActivityViewModel.getResponseV(TV_AIRING_TODAY, 1, { listTvAiringToday ->
+            consumeTvPopular(moviesNowPlaying, moviesPopular, listTvAiringToday?.results)
+        }, { error -> Snackbar.make(coordinatorLayout, "$error", Snackbar.LENGTH_SHORT).show() })
+    }
+
+    private fun consumeTvPopular(
+        moviesNowPlaying: ArrayList<ResultService>?,
+        moviesPopular: ArrayList<ResultService>?,
+        tvAiringToday: ArrayList<ResultService>?
+    ) {
+        homeActivityViewModel.getResponseV(TV_POPULAR, 1, { listTvPopular ->
+            homeActivityViewModel.showFragment(
+                moviesNowPlaying!![0],
+                moviesNowPlaying,
+                moviesPopular,
+                tvAiringToday,
+                listTvPopular?.results,
+                supportFragmentManager.beginTransaction(),
+                appDatabase,
+                this
+            )
+        }, { error -> Snackbar.make(coordinatorLayout, "$error", Snackbar.LENGTH_SHORT).show() })
+    }
+
+    fun setDataResultService(resultService: ResultService?) {
+        ivImageBottom.setImageURI(Uri.parse(BASE_URL_IMAGE + resultService?.poster_path))
+        tvTitleBottom.text = resultService?.title
+        tvDescriptionBottom.text = resultService?.overview
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -41,18 +88,15 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener, MoviesTvShowInte
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE)
 
-        homeActivityViewModel.getResponseV(MOVIES_NOW_PLAYING, 1, { listMoviesNowPlaying ->
-            consumeMoviesPopular(listMoviesNowPlaying?.results)
-        }, {
-
-        })
+        consumeMoviesNowPlaying()
         sheetBehavior = BottomSheetBehavior.from(clDetailBottom)
         sheetBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.btPlay -> Toast.makeText(this, "Reproducir", Toast.LENGTH_LONG).show()
+            R.id.btPlay -> Snackbar.make(coordinatorLayout, "Play any movie", Snackbar.LENGTH_SHORT)
+                .show()
             R.id.tvShows -> homeActivityViewModel.showByType(
                 tvShows, tvMovies, tvMyList,
                 isMovie = false,
@@ -95,46 +139,6 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener, MoviesTvShowInte
     override fun clickMovieTvShow(resultService: ResultService?) {
         this.resultService = resultService
         homeActivityViewModel.clickMovieTvShow(sheetBehavior, resultService, this)
-    }
-
-    private fun consumeMoviesPopular(moviesNowPlaying: ArrayList<ResultService>?) {
-        homeActivityViewModel.getResponseV(MOVIES_POPULAR, 1, { listMoviesPopular ->
-            consumeTvAiringToday(moviesNowPlaying, listMoviesPopular?.results)
-        }, {})
-    }
-
-    private fun consumeTvAiringToday(
-        moviesNowPlaying: ArrayList<ResultService>?,
-        moviesPopular: ArrayList<ResultService>?
-    ) {
-        homeActivityViewModel.getResponseV(TV_AIRING_TODAY, 1, { listTvAiringToday ->
-            consumeTvPopular(moviesNowPlaying, moviesPopular, listTvAiringToday?.results)
-        }, {})
-    }
-
-    private fun consumeTvPopular(
-        moviesNowPlaying: ArrayList<ResultService>?,
-        moviesPopular: ArrayList<ResultService>?,
-        tvAiringToday: ArrayList<ResultService>?
-    ) {
-        homeActivityViewModel.getResponseV(TV_POPULAR, 1, { listTvPopular ->
-            homeActivityViewModel.showFragment(
-                moviesNowPlaying!![0],
-                moviesNowPlaying,
-                moviesPopular,
-                tvAiringToday,
-                listTvPopular?.results,
-                supportFragmentManager.beginTransaction(),
-                appDatabase,
-                this
-            )
-        }, {})
-    }
-
-    fun setDataResultService(resultService: ResultService?) {
-        ivImageBottom.setImageURI(Uri.parse(BASE_URL_IMAGE + resultService?.poster_path))
-        tvTitleBottom.text = resultService?.title
-        tvDescriptionBottom.text = resultService?.overview
     }
 
     override fun onBackPressed() {
